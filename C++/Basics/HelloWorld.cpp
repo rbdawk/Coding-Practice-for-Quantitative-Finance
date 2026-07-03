50b2ee07810cbfcd866a579db95c455acd3d8d12
rbdawk <rbdawk@gmail.com>
2026-07-03 09:17:22 -0500

    First commit

---
+++ C++/Basics/HelloWorld.cpp
@@ -0,0 +1,9 @@
+// Program to print "Hello World," and to also introduce some other basic topics
+
+// Import necessary packages
+#include <iostream>
+
+// Main function, which is executed by the compiled code
+int main(){
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Learning for Quantitative Finance
