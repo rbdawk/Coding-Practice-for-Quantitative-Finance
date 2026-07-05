2fd46ed12e84043094334e7fc5ba5ed8ac956a18
rbdawk <rbdawk@gmail.com>
2026-07-05 16:42:38 -0500

    07/05/26 Updates

--- C++/Basics/HelloWorld.cpp
+++ C++/Basics/HelloWorld.cpp
@@ -1,9 +1,114 @@
+//---------------------------------------------------------
 // Program to print "Hello World," and to also introduce some other basic topics
+//---------------------------------------------------------
+
+// This is a comment
+
+/*
+This is a multi-line comment
+*/
 
 // Import necessary packages
 #include <iostream>
+#include <string>
 
 // Main function, which is executed by the compiled code
 int main(){
     
+    //---------------------------------------------------------
+    // Input & Output
+    //---------------------------------------------------------
+
+    // Console Output
+    // - Print "Hello, world!" to the console and then end the line
+    std::cout << "Hello, world!" << std::endl;
+    
+    //---------------------------------------------------------
+    // Basic Data Types
+    //---------------------------------------------------------
+
+    // Void
+
+    // Booleans
+    bool b = true;
+
+    // Integers
+    // - 9 digits, no decimals allowed.
+    int x = 5;
+
+    // Long Long
+    // - 19 digits, no decimals allowed.
+    long long l = 123456789123456789;
+
+    // Floats
+    // - 7 digits of precision.
+    // - Need the f at the end of a float number, 
+    //   since the compiler compiles the right side of this expression first.
+    float y = 3.14f;
+
+    // Doubles
+    // - 15 digits of precision.
+    double z = 3.1415926535;
+
+    // Characters
+    char a = 'A';
+
+    //---------------------------------------------------------
+    // Compound Data Types
+    //---------------------------------------------------------
+
+    // Strings
+    // - Note, this is not a fundamental datatype
+    std::string message = "Hello, world!";
+
+    // Arrays
+
+    // Vectors
+
+    // Maps
+
+    // Unordered Maps
+
+    // Sets
+
+    // Unordered Sets
+
+    // Pairs
+
+    // Tuples
+
+    // Structs
+
+    // Variants
+
+    // Enumerations
+    
+    //---------------------------------------------------------
+    // If Statements, While Loops, & For Loops
+    //---------------------------------------------------------
+
+    // If Statements
+
+    // While Loops
+
+    // For Loops
+
+    //---------------------------------------------------------
+    // Functions
+    //---------------------------------------------------------
+
+    //---------------------------------------------------------
+    // Classes
+    //---------------------------------------------------------
+
+    //---------------------------------------------------------
+    // Pointers & References
+    //---------------------------------------------------------
+
+    //---------------------------------------------------------
+    // End of Program
+    //---------------------------------------------------------
+
+    // Return 0 to close out the program
+    return 0;
 }
